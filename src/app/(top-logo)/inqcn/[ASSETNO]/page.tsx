a0b93298a99bbdea21e37556c020006243ff7aff
codeeon <yeon.develop@gmail.com>
2024-06-16 14:03:26 +0900

    Design: InQCN 신차 입고, 상세

---
+++ src/app/(top-logo)/inqcn/[ASSETNO]/page.tsx
@@ -0,0 +1,198 @@
+'use client';
+
+import React, { useState } from 'react';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm, Controller } from 'react-hook-form';
+import FormElement from '@/components/form/form-element';
+import FormWrapper from '@/components/form/form-wrapper';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { useParams, useRouter } from 'next/navigation';
+import { qcdata } from '../page';
+import useFetch from '@/hooks/use-fetch';
+
+const InQCNSchema = z.object({
+  ENTRYLOCATION: z.string().nonempty('차량 입고 위치를 선택해 주세요.'),
+  DETAILLOCATION: z.string().optional(),
+  비고: z.string().optional(),
+});
+
+type InQCNSchemaType = z.infer<typeof InQCNSchema>;
+
+const InQCNDetail: React.FC = () => {
+  const params = useParams();
+  const router = useRouter();
+
+  const inQCNForm = useForm<InQCNSchemaType>({
+    resolver: zodResolver(InQCNSchema),
+    defaultValues: {
+      ENTRYLOCATION: '',
+    },
+    mode: 'onChange',
+  });
+
+  //   FIXME: 더미데이터, useFetch 사용 시 주석 처리
+  const selectedData = qcdata.find((data) => data.ASSETNO === params.ASSETNO);
+
+  if (!selectedData) {
+    return <p>해당 데이터가 없습니다.</p>;
+  }
+
+  //   FIXME: useFetch 사용 시 주석 해제
+  //   const {
+  //     data: fetchedData,
+  //     loading,
+  //     error,
+  //     revalidate,
+  //   } = useFetch<QCDataResponse>(`${process.env.NEXT_PUBLIC_API_URL}/InQCN/D`);
+
+  //   if (loading) return <p>Loading...</p>;
+  //   if (error) return <p>Error: {error.message}</p>;
+  //   if (!fetchedData) return <p>No data</p>;
+
+  //   console.log(fetchedData);
+
+  //   const entryLocationList = fetchedData.reqCode[0].HR58;
+  //   const apiData: QCDataType[] = fetchedData.data.data.REPT;
+  //   const selectedData = apiData.find((data) => data.ASSETNO === params.ASSETNO);
+
+  //   if (!selectedData || !entryLocationList) {
+  //     return <p>해당 데이터가 없습니다.</p>;
+  //   }
+
+  // POST 요청 예시
+  const onInQCNFormSubmit = async (data: InQCNSchemaType) => {
+    const formData = new FormData();
+    formData.append('ENTRYLOCATION', data.ENTRYLOCATION);
+    formData.append('DETAILLOCATION', data.DETAILLOCATION || '');
+    formData.append('비고', data.비고 || '');
+
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/InQCN/${params.ASSETNO}`,
+        {
+          method: 'POST',
+          body: formData,
+        }
+      );
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      console.log('요청 완료');
+      const result = await response.json();
+      // TODO: 토스트 메시지 추가, router.push('/inqcn')
+      console.log('Success:', result);
+      // revalidate();
+    } catch (error) {
+      console.error('Fetch error:', error);
+    }
+  };
+
+  return (
+    <div className="px-4 flex justify-center items-center">
+      <Card className="w-full rounded-xl border-slate-200 border-2 mb-24">
+        <CardHeader>
+          <CardTitle>신차 입고 상세 조회</CardTitle>
+          <CardDescription>
+            <span className="ml-0.5 text-[#F31515] text-sm font-medium">*</span>
+            {' 표시된 곳은 반드시 입력해 주셔야 합니다.'}
+          </CardDescription>
+        </CardHeader>
+        <FormWrapper form={inQCNForm} onSubmit={onInQCNFormSubmit}>
+          <CardContent>
+            <div className="flex flex-col gap-4">
+              <div className="flex items-center justify-between h-10">
+                <Label className="font-semibold">차대번호</Label>
+                <p>{selectedData.차대번호}</p>
+              </div>
+              <div className="flex items-center justify-between h-10">
+                <Label className="font-semibold">자산번호</Label>
+                <p>{selectedData.ASSETNO}</p>
+              </div>
+              <div className="flex items-center justify-between h-10">
+                <Label className="font-semibold">모델명</Label>
+                <p>{selectedData.MODEL}</p>
+              </div>
+              <div className="flex items-center justify-between h-10">
+                <Label className="font-semibold">색상</Label>
+                <p>{selectedData.색상}</p>
+              </div>
+            </div>
+            <div className="w-full flex flex-col gap-4 mt-6">
+              <FormElement
+                formControl={inQCNForm.control}
+                name="ENTRYLOCATION"
+                label="차량 입고 위치"
+                required
+              >
+                <Controller
+                  name="ENTRYLOCATION"
+                  control={inQCNForm.control}
+                  render={({ field }) => (
+                    <Select onValueChange={field.onChange} value={field.value}>
+                      <SelectTrigger className="w-full">
+                        <SelectValue placeholder="차량 입고 위치를 선택해 주세요." />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectItem key={'apple'} value={'apple'}>
+                            apple
+                          </SelectItem>
+                          {/* FIXME: useFetch API call */}
+                          {/* {entryLocationList.map((item) => (
+                            <SelectItem key={item} value={item}>
+                              {item}
+                            </SelectItem>
+                          ))} */}
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  )}
+                />
+              </FormElement>
+              <FormElement
+                formControl={inQCNForm.control}
+                name="DETAILLOCATION"
+                label="차량 상세 위치"
+              >
+                <Input placeholder="" className="h-10" />
+              </FormElement>
+              <FormElement
+                formControl={inQCNForm.control}
+                name="비고"
+                label="비고"
+              >
+                <Input placeholder="" className="h-10" />
+              </FormElement>
+            </div>
+          </CardContent>
+          <CardFooter className="fixed bottom-0 left-0 w-full p-4">
+            <Button className="w-full h-12 rounded-lg" type="submit">
+              입고 (QC)
+            </Button>
+          </CardFooter>
+        </FormWrapper>
+      </Card>
+    </div>
+  );
+};
+
+export default InQCNDetail;
--- src/app/(top-logo)/inqcn/page.tsx
+++ src/app/(top-logo)/inqcn/page.tsx
@@ -1,7 +1,189 @@
-import React from 'react';
+'use client';
 
-const Inqcn = () => {
-  return <div>Inqcn</div>;
+import React, { useState } from 'react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { useRouter } from 'next/navigation';
+import useFetch from '@/hooks/use-fetch';
+
+export type QCDataType = {
+  ASSETNO: string;
+  차대번호: string;
+  MODEL: string;
+  색상: string;
 };
 
-export default Inqcn;
+export type QCDataResponse = {
+  data: {
+    result: { MSGE: string; CODE: string };
+    data: { REPT: QCDataType[] };
+  };
+  reqCode: [{ HR58: string[] }, any[]];
+};
+
+export const qcdata: QCDataType[] = [
+  {
+    ASSETNO: 'AST20240700301',
+    차대번호: 'KNAPU81BGRK318499',
+    MODEL: '스포티지',
+    색상: '스노우 화이트 펄',
+  },
+  {
+    ASSETNO: 'AST20240700302',
+    차대번호: 'KNAPU81BGRK318486',
+    MODEL: '스포티지',
+    색상: '스노우 화이트 펄',
+  },
+  {
+    ASSETNO: 'AST20240700303',
+    차대번호: 'KNAPU81BGRK318465',
+    MODEL: '스포티지',
+    색상: '스노우 화이트 펄',
+  },
+  {
+    ASSETNO: 'AST20240700304',
+    차대번호: 'KMHJB81BGRU357155',
+    MODEL: '포터',
+    색상: '오닉스 블루',
+  },
+  {
+    ASSETNO: 'AST20240700305',
+    차대번호: 'KNACG811BRT398243',
+    MODEL: '아반떼',
+    색상: '아마존 그레이 메탈릭',
+  },
+  {
+    ASSETNO: 'AST20240700306',
+    차대번호: 'KNAPU81BGRK318465',
+    MODEL: '포터',
+    색상: '크리미 화이트',
+  },
+  {
+    ASSETNO: 'AST20240700307',
+    차대번호: 'KMFZCZ78BRU146594',
+    MODEL: '아반떼',
+    색상: '아틀라스 화이트',
+  },
+  {
+    ASSETNO: 'AST20240700308',
+    차대번호: 'KMFZCZ78BRU145759',
+    MODEL: '래이',
+    색상: '클리어 화이트',
+  },
+  {
+    ASSETNO: 'AST20240700308',
+    차대번호: 'KMFZCZ78BRU145760',
+    MODEL: '팰리세이드',
+    색상: '사파이어 블랙 아쿠아',
+  },
+];
+
+const InQCN: React.FC = () => {
+  const router = useRouter();
+
+  const [search, setSearch] = useState('');
+  const [selectedASSETNO, setSelectedASSETNO] = useState<string>('');
+
+  // FIXME: 아래 코드는 실제 API 데이터를 받아오는 코드입니다.
+  // const {
+  //   data: response,
+  //   loading,
+  //   error,
+  //   revalidate,
+  // } = useFetch<QCDataResponse>(`${process.env.NEXT_PUBLIC_API_URL}/InQCN/D`);
+
+  // if (loading) return <p>Loading...</p>;
+  // if (error) return <p>Error: {error.message}</p>;
+  // if (!response) return <p>No data</p>;
+
+  // console.log(response);
+
+  // FIXME: 아래 코드는 실제 API 데이터를 받아오는 코드입니다.
+  // const apiData = response.data.data.REPT;
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const handleRowClick = (e: React.MouseEvent<HTMLTableRowElement>) => {
+    const assetno = e.currentTarget.getAttribute('data-assetno');
+    const data = qcdata.find((data) => data.ASSETNO === assetno);
+    // FIXME: 아래 코드는 실제 API 데이터를 받아오는 코드입니다.
+    // const data = apiData.find((datum) => datum.ASSETNO === assetno);
+    setSelectedASSETNO(data?.ASSETNO || '');
+  };
+
+  const handleDetailClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (selectedASSETNO) {
+      router.push(`/inqcn/${selectedASSETNO}`);
+    }
+  };
+
+  const filteredData = qcdata.filter((datum) => {
+    // FIXME: 아래 코드는 실제 API 데이터를 받아오는 코드입니다.
+    // const filteredData = apiData.filter((datum) => {
+    return datum.차대번호.includes(search);
+  });
+
+  return (
+    <article className="px-4 relative">
+      <h1 className="text-center font-medium text-xl my-8 text-[#1B1B1B]/90">
+        신차 입고 QC
+      </h1>
+      <div className="flex flex-col my-4 gap-4">
+        <Input
+          placeholder="차대번호로 검색하기"
+          name="search"
+          value={search}
+          onChange={handleSearch}
+        />
+      </div>
+      <Table className="mb-24">
+        <TableHeader>
+          <TableRow className="bg-green-500/50 hover:bg-green-500/35">
+            <TableHead className="text-center rounded-tl-lg px-1">
+              차량번호
+            </TableHead>
+            <TableHead className="text-center px-1">고객명</TableHead>
+            <TableHead className="text-center rounded-tr-lg px-1">
+              입고사유
+            </TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {filteredData.map((data) => (
+            <TableRow
+              key={data.ASSETNO}
+              data-assetno={data.ASSETNO}
+              onClick={handleRowClick}
+              className={
+                data.ASSETNO === selectedASSETNO ? 'text-primary/80' : ''
+              }
+            >
+              <TableCell className="font-medium text-center px-1">
+                {data.차대번호}
+              </TableCell>
+              <TableCell className="text-center px-1">{data.MODEL}</TableCell>
+              <TableCell className="text-center px-1">{data.색상}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <div className="flex fixed bottom-4 left-0 w-full px-4">
+        <Button className="w-full h-12 rounded-lg" onClick={handleDetailClick}>
+          입력
+        </Button>
+      </div>
+    </article>
+  );
+};
+
+export default InQCN;
