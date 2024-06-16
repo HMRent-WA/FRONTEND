20a5cd65a9dc39f591c56ed66562cfe3b8c47d25
codeeon <yeon.develop@gmail.com>
2024-06-16 19:21:03 +0900

    Design: Loading Page 기반 제작, 수정 예정

---
+++ src/components/loading.tsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+// const LoadingPage = () => {
+//   return (
+//     <div className="flex justify-center items-center h-screen">
+//       <div className="loader">로딩 중</div>
+//     </div>
+//   );
+// };
+
+// export default LoadingPage;
