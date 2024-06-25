032c00e2e6e70d2ea380a61ea4afc8ea7149ce62
codeeon <yeon.develop@gmail.com>
2024-06-25 23:48:28 +0900

    Feat: 로그인 확인 처리(GET auth) 시, 쿠키 전송

--- src/app/(top-logo)/layout.tsx
+++ src/app/(top-logo)/layout.tsx
@@ -11,17 +11,26 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   // const [isLoading, setIsLoading] = React.useState<boolean>(true);
 
   useEffect(() => {
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth`)
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+    })
       .then((response) => response.json())
       .then((data) => {
+        console.log('auth -> ', data, data.message);
         if (data.message === 'FAIL') {
-          router.push('/login');
+          // showErrorToast('로그인 후 이용해주세요.');
+          // router.push('/login');
         }
       })
       .catch((err) => {
         // TODO: 아래 주석 해제해야 비로그인 접근 제어 가능
-        // router.push('/login');
         // showErrorToast('로그인 후 이용해주세요.');
+        // router.push('/login');
+        console.error('Auth failed:', err);
       });
   }, [router]);
 
